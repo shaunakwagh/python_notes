b90dd3baf21a0b05f0caea2f7f8223d6d2f55d57
Shaunak Wagh <shaunakwagh@gmail.com>
2025-10-01 00:32:28 -0400

    tilda and bitwise

---
+++ Python_knowlegde/tilda.py
@@ -0,0 +1,15 @@
+"""
+This code demonstrates the use of the bitwise NOT operator (~) in Python.
+
+When used as an operator, the tilde symbol in Python represents
+the bitwise complement or bitwise NOT operator.
+This operator inverts the bits of an integer.
+When applied to a number x, the result is -(x + 1)
+
+"""
+
+
+
+x = 5
+result = ~x
+print(result) # Output: -6
